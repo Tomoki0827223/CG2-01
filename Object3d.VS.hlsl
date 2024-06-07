b1481f3cd8ec8879fd78397c21234e8ce8a081af
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-06-07 16:49:29 +0900

    ggfgg

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -3,24 +3,22 @@
 struct TransformationMatrix
 {
     float32_t4x4 WVP;
+
 };
 ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShaderInput
 {
-    float32_t4 position : POSITION;
+    float32_t4 position : POSITION0;
     float32_t2 texcoord : TEXCOORD0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
+
     VertexShaderOutput output;
-
-    // 頂点の位置をワールドビュー射影行列で変換
-    output.position = mul(input.position, gTransformationMatrix.WVP);
-
-    // テクスチャ座標をそのまま渡す
-    output.texcood = input.texcoord;
     
+    output.position = mul(input.position, gTransformationMatrix.WVP);
+    output.texcoord = input.texcoord;
     return output;
-}
+};
