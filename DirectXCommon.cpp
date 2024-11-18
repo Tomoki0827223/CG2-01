987f0dcbb079f312a3a6475b94789d9861a8a73c
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-11-18 13:57:36 +0900

    ｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆｆ

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -2,21 +2,35 @@
 
 
 void DirectXCommon::Initialize(WinApp* winApp) {
-    assert(winApp); // NULL チェック
+    // NULL検出
+    assert(winApp);
+    // メンバ変数に記録
     this->winApp_ = winApp;
 
+    // デバイスの生成
     CreateDevice();
+    // コマンド関連の初期化
     InitializeCommandObjects();
+    // スワップチェーンの生成
     CreateSwapChain();
+    // 深度バッファの生成
     CreateDepthBuffer();
+    // 各種デスクリプタヒープの生成
     CreateDescriptorHeaps();
+    // レンダーターゲットビューの初期化
     InitializeRenderTargetView();
+    // 深度ステンシルビューの初期化
     InitializeDepthStencilView();
+    // フェンスの初期化
     InitializeFence();
+    // ビューポート矩形の初期化
     InitializeViewportAndScissorRect();
+    // DXCコンパイラの生成
     CreateDXCCompiler();
+    // ImGuiの初期化
     InitializeImGui();
 }
+
 
 void DirectXCommon::CreateDevice() {
 
