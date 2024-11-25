138426cd5839236716b57db84c0f8762260c131d
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-11-25 14:56:35 +0900

    cffff

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -2,21 +2,34 @@
 #include <dxcapi.h>
 #include <cassert>
 #include <combaseapi.h>
+#include <d3d12.h>
 
-void DirectXCommon::Initialize()
-{
-	//DXCの初期化
-	IDxcUtils* dxcUtils = nullptr;
-	IDxcCompiler3* dxcCompiler = nullptr;
+void DirectXCommon::Initialize(WinApp* winApp) {
+    // NULL検出
+    assert(winApp);
+    // メンバ変数に記録
+    this->winApp_ = winApp;
 
-	hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
-	assert(SUCCEEDED(hr));
-
-	hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
-	assert(SUCCEEDED(hr));
-
-	//include対応のため設定しておく
-	IDxcIncludeHandler* includeHandler = nullptr;
-	hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
-	assert(SUCCEEDED(hr));
+    // デバイスの生成
+    CreateDevice();
+    // コマンド関連の初期化
+    InitializeCommandObjects();
+    // スワップチェーンの生成
+    CreateSwapChain();
+    // 深度バッファの生成
+    CreateDepthBuffer();
+    // 各種デスクリプタヒープの生成
+    CreateDescriptorHeaps();
+    // レンダーターゲットビューの初期化
+    InitializeRenderTargetView();
+    // 深度ステンシルビューの初期化
+    InitializeDepthStencilView();
+    // フェンスの初期化
+    InitializeFence();
+    // ビューポート矩形の初期化
+    InitializeViewportAndScissorRect();
+    // DXCコンパイラの生成
+    CreateDXCCompiler();
+    // ImGuiの初期化
+    InitializeImGui();
 }
--- DirectXCommon.h
+++ DirectXCommon.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "WinApp.h"
+
 class DirectXCommon
 {
 public:
 
-	void Initialize();
+	void Initialize(WinApp* winApp);
 
 };
 
