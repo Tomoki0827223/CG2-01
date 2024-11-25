f0b618209613b2e3ba6d276e12f2efb2702d1a72
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-11-25 17:10:11 +0900

    addawda

--- StringUitilty.h
+++ StringUitilty.h
@@ -1,14 +1,13 @@
-#pragma once
-#include <string>
-#include <stringapiset.h>
-
 #ifndef _AMD64_
 #define _AMD64_
 #endif
+
+
+#include <string>
+#include <stringapiset.h>
 
 namespace StringUitilty
 {
 	std::wstring ConvertString(const std::string& str);
 	std::string ConvertString(const std::wstring& str);
 };
-
