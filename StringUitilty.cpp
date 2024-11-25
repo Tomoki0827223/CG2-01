7d6636bb6034d61f26f5ab09cc172ef35619c7ae
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-11-25 17:09:39 +0900

    fdheragagr

--- StringUitilty.cpp
+++ StringUitilty.cpp
@@ -2,32 +2,42 @@
 
 namespace StringUitilty {
 
-	std::wstring ConvertString(const std::string& str) {
-		if (str.empty()) {
-			return std::wstring();
-		}
+    std::wstring ConvertString(const std::string& str)
+    {
+        if (str.empty()) {
+            return std::wstring();
+        }
 
-		auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-		if (sizeNeeded == 0) {
-			return std::wstring();
-		}
-		std::wstring result(sizeNeeded, 0);
-		MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-		return result;
-	}
+        // 必要なサイズを取得
+        int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
+        if (sizeNeeded == 0) {
+            return std::wstring(); // 変換に失敗
+        }
 
-	std::string ConvertString(const std::wstring& str) {
-		if (str.empty()) {
-			return std::string();
-		}
+        // 結果の wstring を作成
+        std::wstring result(sizeNeeded, L'\0');
+        MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), &result[0], sizeNeeded);
 
-		auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-		if (sizeNeeded == 0) {
-			return std::string();
-		}
-		std::string result(sizeNeeded, 0);
-		WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-		return result;
-	}
+        return result;
+    }
+
+    std::string ConvertString(const std::wstring& str)
+    {
+        if (str.empty()) {
+            return std::string();
+        }
+
+        // 必要なサイズを取得
+        int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0, nullptr, nullptr);
+        if (sizeNeeded == 0) {
+            return std::string(); // 変換に失敗
+        }
+
+        // 結果の string を作成
+        std::string result(sizeNeeded, '\0');
+        WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), &result[0], sizeNeeded, nullptr, nullptr);
+
+        return result;
+    }
 
 }
