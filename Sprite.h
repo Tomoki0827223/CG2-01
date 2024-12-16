d66e5cf7894d616994016a3d11c5604f3fef77ae
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-12-16 14:12:12 +0900

    eeee

--- Sprite.h
+++ Sprite.h
@@ -48,35 +48,35 @@ private:
 
 	SpriteCommon* spriteCommon_ = nullptr;
 
-	////頂点リソースを作る
-	//Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource;
-	////インデックスリソースを作る
-	//Microsoft::WRL::ComPtr<ID3D12Resource> indexResource;
+	//頂点リソースを作る
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource;
+	//インデックスリソースを作る
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResource;
 
-	////VertexBufferResourceを生成
-	//Microsoft::WRL::ComPtr<ID3D12Resource> vertexResourceSprite;
-	////IndexBufferResourceを生成
-	//Microsoft::WRL::ComPtr<ID3D12Resource> indexResourceSprite;
+	//VertexBufferResourceを生成
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResourceSprite;
+	//IndexBufferResourceを生成
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResourceSprite;
 
-	//VertexData* vertexData = nullptr;
-	//uint32_t* indexData = nullptr;
-	//
-	////頂点バッファビューを作成する
-	//D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	////インデックスリソースを作る
-	//D3D12_INDEX_BUFFER_VIEW indexBufferView{};
+	VertexData* vertexData = nullptr;
+	uint32_t* indexData = nullptr;
+	
+	//頂点バッファビューを作成する
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	//インデックスリソースを作る
+	D3D12_INDEX_BUFFER_VIEW indexBufferView{};
 
-	////マテリアルリソースを作る//Material用のResourceを作る
-	//Microsoft::WRL::ComPtr<ID3D12Resource> materialResource;
-	////マテリアルのポインター
-	//Material* materialData = nullptr;
+	//マテリアルリソースを作る//Material用のResourceを作る
+	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource;
+	//マテリアルのポインター
+	Material* materialData = nullptr;
 
-	//Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> materialHeap;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> materialHeap;
 
-	////バッファリソースを作る
-	//Microsoft::WRL::ComPtr<ID3D12Resource> transformationMatrixResource;
-	////バッファリソース内のデータを指すポインタ
-	//TransformationMatrix* transformationMatrix = nullptr;
+	//バッファリソースを作る
+	Microsoft::WRL::ComPtr<ID3D12Resource> transformationMatrixResource;
+	//バッファリソース内のデータを指すポインタ
+	TransformationMatrix* transformationMatrix = nullptr;
 
 
 };
